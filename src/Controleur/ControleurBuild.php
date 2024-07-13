1db620038be7447d38c2fbecca3c0347ad978ec4
march <marc.haye@etu.umontpellier.fr>
2024-07-13 11:41:50 +0200

    ajout DataObject/Build et ControleurBuild

---
+++ src/Controleur/ControleurBuild.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\EldenBuild\Controleur;
+
+class ControleurBuild
+{
+
+}
---
+++ src/Modele/DataObject/Build.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\EldenBuild\Modele\DataObject;
+
+class Build extends AbstractDataObject
+{
+
+    public function formatTableau(): array
+    {
+        // TODO: Implement formatTableau() method.
+    }
+
+    public static function construireDepuisFormulaire(array $tableauFormulaire): AbstractDataObject
+    {
+        // TODO: Implement construireDepuisFormulaire() method.
+    }
+}
