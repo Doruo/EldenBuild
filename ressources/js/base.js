3b3756d602ad876b851428368a8ce1f02fb10800
march <marc.haye@etu.umontpellier.fr>
2024-07-14 00:09:10 +0200

    ajustement js .webp et ajout changeBackground

--- ressources/js/base.js
+++ ressources/js/base.js
@@ -5,24 +5,28 @@
 const IMAGES_BACKGROUNDS_URL = "/ressources/images/backgrounds";
 
 const images = [
-    `${IMAGES_BACKGROUNDS_URL}/liurnia.png`,
-    `${IMAGES_BACKGROUNDS_URL}/dephts.png`,
-    `${IMAGES_BACKGROUNDS_URL}/wyvern.png`
+    `${IMAGES_BACKGROUNDS_URL}/liurnia.webp`,
+    `${IMAGES_BACKGROUNDS_URL}/dephts.webp`,
+    `${IMAGES_BACKGROUNDS_URL}/wyvern.webp`
 ];
 
 let indexImages = 0;
-changeBackGround();
+changeBackGround("dephts");
 
 // Defilement arrière-plan
-//const chb = setInterval(changeBackGround, 20000);
+//const chb = setInterval(changeBackGroundSuivant, 20000);
 
-function changeBackGround(){
+function changeBackGroundSuivant(){
     document.body.style.backgroundImage = `url('${images[indexImages]}')`;
     indexImages = (indexImages + 1) % images.length;
 }
 
+function changeBackGround(nom){
+    document.body.style.backgroundImage = `url('${IMAGES_BACKGROUNDS_URL}/${nom}.webp')`;
+}
+
 function lancerDefilement(){
-    const chb = setInterval(changeBackGround, 20000);
+    const chb = setInterval(changeBackGroundSuivant, 20000);
 }
 
 function stopperDefilement(){
