bc4e8d98d4673d7d91d27f6877b6dcf78902f337
march <marc.haye@etu.umontpellier.fr>
2024-07-13 19:10:53 +0200

    correctif table user.emailAValider

--- ressources/sql/creationTables.sql
+++ ressources/sql/creationTables.sql
@@ -2,7 +2,7 @@ CREATE TABLE Users(
                       login VARCHAR(50),
                       mdpHache VARCHAR(256) NOT NULL,
                       email VARCHAR(50) NOT NULL,
-                      emailAVerifier VARCHAR(50) NOT NULL,
+                      emailAValider VARCHAR(50) NOT NULL,
                       nonce VARCHAR(50) NOT NULL,
                       estAdmin BOOLEAN NOT NULL,
                       PRIMARY KEY(login)
