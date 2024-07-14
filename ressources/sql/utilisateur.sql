6236b86b0389e10fac8b6c0198fdca4b378758d0
march <marc.haye@etu.umontpellier.fr>
2024-07-14 13:36:18 +0200

    deplacement des scripts sql hors site

--- ressources/sql/utilisateur.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users(
-                      login VARCHAR(50),
-                      mdpHache VARCHAR(256) NOT NULL,
-                      email VARCHAR(50) NOT NULL,
-                      emailAValider VARCHAR(50) NOT NULL,
-                      nonce VARCHAR(50) NOT NULL,
-                      estAdmin BOOLEAN NOT NULL,
-                      PRIMARY KEY(login)
-);
